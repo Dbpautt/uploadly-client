02a65efc7374d6920937fb3addc9a00a26adad59
Barbara Koehler <barkoehler@gmail.com>
2018-10-11 07:08:32 -0400

    fixed some stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
-import {PdfViewerModule} from 'ng2-pdf-viewer';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 //----components---//
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { SignupComponent } from './pages/signup/signup.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { LoginPageComponent } from './pages/login/login.component';
 
+const routes: Routes = [
+  { path: '', component: AppComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: 'login', component: LoginPageComponent},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
@@ -21,9 +28,10 @@ import { LoginPageComponent } from './pages/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot(routes),
     PdfViewerModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
