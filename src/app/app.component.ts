ee6635ada58a089be67300c22593cf53ca36361f
Barbara Koehler <barkoehler@gmail.com>
2018-10-13 13:32:34 -0400

    file upload continued

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent implements OnInit {
 
   logout() {
     this.authService.logout()
-      .then(() => this.router.navigate(['/login']));
+      .then(() => this.router.navigate(['/']));
   }
 }
 
